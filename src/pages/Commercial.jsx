bd337d3434a01e50b7f2e9155b863e8c06495448
Nitin_host <nitinravella02@gmail.com>
2025-05-05 00:54:04 +0530

    home page

---
+++ src/pages/Commercial.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Commercial() {
+    return (
+        <div className="commercial">
+        <h1>Commercial Services</h1>
+        <p>This is the commercial services page of our application.</p>
+        </div>
+    );
+}
